ab70fe1d4ad3f5e913ddf32227f258d38b4e8a50
SPO36 <sage.park@obigo.com>
2025-06-17 08:42:54 +0900

    add: audio file

--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -5,13 +5,33 @@ function EpisodeCard({ title, creator, src, id, themeSlug, audioFile }) {
   const navigate = useNavigate();
 
   const handlePlayClick = () => {
-    if (!audioFile) return; // 오디오 없으면 무시
+    if (!audioFile) {
+      showToast(title + '의 오디오 파일이 존재하지 않습니다');
+      e.stopPropagation(); // 카드 클릭 막기
+    }
     if (themeSlug) {
       navigate(`/episode/${id}/${themeSlug}`);
     } else {
       navigate(`/episode/${id}`);
     }
   };
+
+  function showToast(message = 'test') {
+    const toast = document.createElement('div');
+    toast.className =
+      'toast toast-top toast-end z-50 fixed top-4 right-4 transition-opacity duration-300';
+    toast.innerHTML = `
+      <div class="shadow-lg text-white alert alert-error">
+        <span>${message}</span>
+      </div>
+    `;
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+      toast.classList.add('opacity-0');
+      setTimeout(() => toast.remove(), 500);
+    }, 2000);
+  }
 
   return (
     <div className="cursor-pointer" onClick={handlePlayClick}>
